568ff08345e05d977c38f9ac673691166dfbf861
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-16 18:19:42 -0300

    criação dos models

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 }
